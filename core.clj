a7bca8ce364fc5d27474c9e7e0945b87e293b271
Pulkit Ghai <39457346+PGhai@users.noreply.github.com>
2019-05-29 17:06:56 -0400

    Update core.clj

--- core.clj
+++ core.clj
@@ -5,37 +5,20 @@
   (doseq [n arr]
     (doseq [x n]
       (print x))
-    (println))
-  
-  
-  )
-
-(defn col[treasureMap]
-  (def col (alength (aget treasureMap 0)))
-  col
-  )
-
-(defn row[treasureMap]
- (def r (alength treasureMap))
-  r
-  )
-
-
-
+    (println)))
 
 (defn solvePathUtil
   [treasureMap x y sol row col]
   (def true-flag 0)
   (def false-flag 1)
-  
   (if(and (>= x 0) (>= y 0) (< x row) (< y col))
 	  (do
      (case (aget treasureMap x y)
-	      "@" (do(println "found")
+	      "@" (do(println "Woo hoo, I found the treasure :-)")
              (def ret true-flag))
 	      "-" (do             
-             (if(not= (aget sol x y) "x")
-               (do(aset sol x y "x")
+             (if(not= (aget sol x y) "+")
+               (do(aset sol x y "+")
                  
                  (if(=(solvePathUtil treasureMap x (inc y) sol row col) true-flag)
                    (def ret true-flag)
@@ -47,31 +30,26 @@
                             (def ret true-flag)
                             (do(aset sol x y "!")
                               (def ret false-flag))
-                            ) 
+                      ) 
                      ) 
+                    )
                    )
-                 )
                   )
                  )
-               )
-             
-             
+                )
         "#" (def ret false-flag)
 	      )
-     )
-   
+      )
    )
   ret
   )
-
-(defn strlen-exceeds? [s n]
-  (> (count s) n))
-
 
 (defn find-path
   [treasureMap row col sol]
   (if(= (solvePathUtil treasureMap 0 0 sol row col) 0)
     (printSol sol)
+    (do(println "Uh oh, I could not find the treasure :-(")
+      (printSol sol))
     )
   )
 
@@ -80,7 +58,6 @@
    (with-open [rdr (clojure.java.io/reader "map.txt")]
    (reduce conj [] (line-seq rdr))
    ))
-
 
 (defn -main
   [&args]
@@ -91,9 +68,9 @@
   (def sol (to-array-2d data))
   (def row (alength treasureMap))
   (def col (alength (aget treasureMap 0)))
+  (println "This is my challenge")
+  (printSol treasureMap)
 	(find-path treasureMap row col sol)
-
-
 	nil
 )
 
