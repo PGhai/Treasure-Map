b6ea304cda9a79a7a6824473e3b37ef7ee88a2d9
Pulkit Ghai <39457346+PGhai@users.noreply.github.com>
2019-05-29 11:44:37 -0400

    Update core.clj

--- core.clj
+++ core.clj
@@ -25,30 +25,43 @@
 
 (defn solvePathUtil
   [treasureMap x y sol row col]
+  (def true-flag 0)
+  (def false-flag 1)
+  
   (if(and (>= x 0) (>= y 0) (< x row) (< y col))
 	  (do
      (case (aget treasureMap x y)
-	      "@" (println "found")
+	      "@" (do(println "found")
+             (def ret true-flag))
 	      "-" (do             
              (if(not= (aget sol x y) "x")
                (do(aset sol x y "x")
                  
-                 (solvePathUtil treasureMap x (inc y) sol row col)
-                 (solvePathUtil treasureMap (inc x) y sol row col)
-                 (solvePathUtil treasureMap (dec x) y sol row col)
-                 (solvePathUtil treasureMap  x (dec y) sol row col)
-                 
+                 (if(=(solvePathUtil treasureMap x (inc y) sol row col) true-flag)
+                   (def ret true-flag)
+                   (if(= (solvePathUtil treasureMap (inc x) y sol row col) true-flag)
+                     (def ret true-flag)
+                     (if(= (solvePathUtil treasureMap (dec x) y sol row col) true-flag)
+                        (def ret true-flag)
+                        (if(=(solvePathUtil treasureMap  x (dec y) sol row col) true-flag)
+                            (def ret true-flag)
+                            (do(aset sol x y "!")
+                              (def ret false-flag))
+                            ) 
+                     ) 
                    )
+                 )
+                  )
                  )
                )
              
              
-        "#" (def ret 0)
+        "#" (def ret false-flag)
 	      )
      )
    
    )
-  0
+  ret
   )
 
 (defn strlen-exceeds? [s n]
